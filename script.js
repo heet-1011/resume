68da182a4ee9e198202898eb5bcebce1d33456a6
Heet Patel <75671020+heet-1011@users.noreply.github.com>
2023-12-12 14:40:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ let api = "https://script.google.com/macros/s/AKfycbwGEAJH7DzT8hSl4ATScTnuemnyfp
 $(document).ready(function () {
    $.getJSON('https://geolocation-db.com/json/')
       .done(function (location) {
-         console.log(location.country_name);
-         console.log(location.state);
-         console.log(location.city);
-         console.log(location.latitude);
-         console.log(location.longitude);
-         console.log(location.IPv4);
          var accessTime = new Date().toISOString();
          let obj = {
             time: accessTime,
@@ -23,7 +17,7 @@ $(document).ready(function () {
             body: JSON.stringify(obj)
          }).then(res => res.text())
             .then(data => {
-               alert(data)
+               console.log("Welcome");
             });
       });
 })
