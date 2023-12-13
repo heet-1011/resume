1065b239bd8b2a0a76f96fe1efc4738a2b62f29a
Heet Patel <75671020+heet-1011@users.noreply.github.com>
2023-12-13 11:12:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let api = "https://script.google.com/macros/s/AKfycbwGEAJH7DzT8hSl4ATScTnuemnyfplRe8dhF8n2mXj0ZFkvh6bcv7zWFuoAYpA6Sd0/exec";
+let api = "https://script.google.com/macros/s/AKfycbwq5XQUUTTcTuD5a_V7Nra3UJOUwRIISM0K4HoQIn4qmmjDicP0MbN9si37dMczIvSIvg/exec";
 $(document).ready(function () {
    $.getJSON('https://geolocation-db.com/json/')
       .done(function (location) {
+         var userAgent = navigator.userAgent;
+         var battery = navigator.getBattery();
          var accessTime = new Date().toISOString();
-         let obj = {
-            time: accessTime,
-            country: location.country_name,
-            state: location.state,
-            city: location.city,
-            latitude: location.latitude,
-            longitude: location.longitude,
-            ipv4: location.IPv4
-         };
-         fetch(api, {
-            method: "POST",
-            body: JSON.stringify(obj)
-         }).then(res => res.text())
-            .then(data => {
-               console.log("Welcome");
+         battery
+            .then(batteryinfo => {
+               let obj = {
+                  time: accessTime,
+                  country: location.country_name,
+                  state: location.state,
+                  city: location.city,
+                  latitude: location.latitude,
+                  longitude: location.longitude,
+                  ipv4: location.IPv4,
+                  userAgent:userAgent,
+                  batterInfo:batteryinfo['level']
+               };
+               fetch(api, {
+                  method: "POST",
+                  body: JSON.stringify(obj)
+               }).then(res => res.text())
+                  .then(data => {
+                     console.log("Welcome")
+                  });
             });
       });
 })
